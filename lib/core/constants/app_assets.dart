8f568d5db0c1305c815769f0de8dd92362953076
Zeina <zeina.wael735@gmail.com>
2026-07-23 23:19:33 +0300

    feat(core): setup core UI, assets configuration, and native splash screen
    - Configure assets directory structure for images and icons in pubspec.yaml
    - Add AppAssets class in core/constants with static constants for asset paths
    - Setup flutter_native_splash package for native Android and iOS splash screens
    - Define AppColors and AppTheme in core/theme for light theme consistency
    - Add AppRoutes class in core/route with static constants for navigation routes

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,17 @@
+ abstract class AppAssets {
+
+  static const String firstOnboarding="assets/image/onboarding_1.png";
+  static const String secondOnboarding="assets/image/onboarding_2.png";
+  static const String helloImage="assets/image/hello_image.png";
+  static const String shoppingCart="assets/image/shopping_cart.png";
+  static const String heartFavouriteScreen="assets/image/heart_favourite_screen.png";
+
+  static const String cartIcon="assets/icon/cart.svg";
+  static const String favouriteIcon="assets/icon/favourite.svg";
+  static const String homeIcon="assets/icon/home.svg";
+  static const String profileIcon="assets/icon/profile.svg";
+  static const String magnifierIcon="assets/icon/magnifier.svg";
+
+
+
+}
---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,21 @@
+abstract class Routes{
+
+  static const String homeScreen="/homeScreen";
+  static const String logInAndSignUpScreen="/logInAndSignUpScreen";
+  static const String logInScreen="/logInScreen";
+  static const String signUpScreen="/signUpScreen";
+  static const String cartScreen="/cartScreen";
+  static const String productOfCategoryScreen="/productOfCategoryScreen";
+  static const String productDetailsScreen="/productDetailsScreen";
+  static const String productSearchScreen="/productSearchScreen";
+  static const String cartEmptyConditionScreen="/cartEmptyConditionScreen";
+  static const String favouriteScreen="/favouriteScreen";
+  static const String favouriteEmptyConditionScreen="/favouriteEmptyConditionScreen";
+  static const String firstOnBoardingScreen="/onBoardingScreen";
+  static const String secondOnBoardingScreen="/secondOnBoardingScreen";
+  static const String accountScreen="/accountScreen";
+
+
+
+
+}
