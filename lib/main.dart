05b67b194804504a34b14dfc80d6b084ee0466f6
dell <mariamnagm1692006@gmail.com>
2026-07-30 13:24:16 +0300

    -merge feature account to dev and solve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/core/model/item/product_item_entity.dart';
-import 'package:shopping_app/feature/auth/presentation/view/screens/sign_up_screen.dart';
-import 'package:shopping_app/feature/category/view/screens/product_details_screen.dart';
-import 'package:shopping_app/feature/home/presentation/view/screens/home_screen.dart';
 import 'core/di/service_locator.dart';
 import 'core/routes/app_routers.dart';
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
-import 'feature/auth/presentation/view/screens/login_screen.dart';
 void main()  {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
